a1ae342de9e0228e45bba2391b8d6cf0635f3227
Fabricio Catae <fcatae@microsoft.com>
2017-05-15 15:56:53 -0700

    add receiver endpoint

---
+++ Arda.Crawler/Controllers/ReceiverController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Arda.Crawler.Controllers
+{
+    [Route("api/[controller]")]
+    public class ReceiverController : Controller
+    {
+        static List<string> _messages = new List<string>();
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return _messages;
+        }
+
+        [HttpGet("add")]
+        public void Get([FromQuery]string value)
+        {
+            _messages.Add(value);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+            _messages.Add(value);
+        }
+       
+    }
+}
